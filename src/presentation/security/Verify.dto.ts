d0074c572fe9967ddafdd503da982f73666021e8
Ivan Rodriguez <irda.devs@gmail.com>
2026-02-14 11:02:28 +0100

    [FIX]: Fixed an issue with verify DTO, was length(6) and now is (8).

--- src/presentation/security/Verify.dto.ts
+++ src/presentation/security/Verify.dto.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const VerifyDTO = z.object({
   email: z.email(),
-  code: z.string().length(6),
+  code: z.string().length(8),
 });
 
 export type VerifyDTO = z.infer<typeof VerifyDTO>;
